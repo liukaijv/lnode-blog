f9f79b396f7cf787d3b210e942d055c048c84c4c
guoxuemeng <89016230@qq.com>
2018-04-18 16:10:47 +0800

    add cross-evn

--- models/post.js
+++ models/post.js
@@ -6,23 +6,23 @@ var updatedAt = require('./plugins').updatedAt;
 
 var PostSchema = new Schema({
   title:  String,
-  slug:  String, 
+  slug:  String,
   summary: String,
   content: String,
   content_raw: String,
   cover_image: String,
   project_link: String,
   attachment: String,
-  category: {type: ObjectId, ref: 'Category', childPath: 'posts'}, 
+  category: {type: ObjectId, ref: 'Category', childPath: 'posts'},
   user: {type: ObjectId, ref:'User'},
-  tags: [{type: ObjectId, ref: 'Tag', childPath: 'posts'}], 
+  tags: [{type: ObjectId, ref: 'Tag', childPath: 'posts'}],
   author: String,
-  source: {type: String, default: 'original'}, //original,reprinted,translated  
+  source: {type: String, default: 'original'}, //original,reprinted,translated
   status: {type: String, default: 'publish'}, //draft,publish
   is_markdown: {type: Boolean, default: true },
-  is_hidden: {type: Boolean, default: false },
+  is_hidden: {type: Boolean, default: true },
   allow_comment: {type: Boolean, default: true },
-  allow_feed: {type: Boolean, default: true }, 
+  allow_feed: {type: Boolean, default: true },
   hits: {type: Number, default: 0},
   comments: {type: Number, default: 0},
   created_at: {type: Date, default: Date.now },
